c61b57d4776ebfc1ac27ba450e8a980b45bf3a20
Jayhuang520 <zhhu8732@colorado.edu>
2018-01-26 17:45:18 -0700

    Added Junit test for Customer.java class

---
+++ test/com/HuangsHotelReservation/model/domain/CustomerTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.HuangsHotelReservation.model.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author JayHu
+ *
+ */
+class CustomerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
