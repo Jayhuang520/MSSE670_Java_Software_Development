159beb74e49e03e9bf54ae62151ec8fc91e0d0f8
Jayhuang520 <zhhu8732@colorado.edu>
2018-02-06 21:42:47 -0700

    Added author name in CalendarServiceImpl

--- src/com/HuangsHotelReservation/model/services/calendarservice/CalendarServiceImpl.java
+++ src/com/HuangsHotelReservation/model/services/calendarservice/CalendarServiceImpl.java
@@ -3,6 +3,11 @@ package com.HuangsHotelReservation.model.services.calendarservice;
 import com.HuangsHotelReservation.model.domain.City;
 import com.HuangsHotelReservation.model.domain.Room;
 
+/**
+ * 
+ * @author Jay Huang
+ *
+ */
 public class CalendarServiceImpl implements ICalendarService{
 	public boolean RetrieveAvaliableRoom(City city,Room room) 
 	{
